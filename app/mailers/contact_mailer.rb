d40f4d67ee97f80f3f5ce31f06924ff6c8d00361
sachio <sachio.engineer@gmail.com>
2021-03-09 23:01:07 +0900

    TOメール変更

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: ENV["MAILADDRESS"], subject: "お問合せ確認メール"
+    mail to: "suzuki.kintai@gmail.com", subject: "お問合せ確認メール"
   end
 end
